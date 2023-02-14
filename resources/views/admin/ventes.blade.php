843b3f6ce66938a41e7b3fe2066311f49f423812
theMaz <patriczum@gmail.com>
2023-02-14 17:24:51 +0200

    345678765432

--- resources/views/admin/ventes.blade.php
+++ resources/views/admin/ventes.blade.php
@@ -1181,17 +1181,24 @@
                             socketIO.on("new-item", (data) => {
                                 var dev = (data.prix.split(' ')).slice(-1)[0];
                                 var prix = data.prix.replace(` ${dev}`, '');
-                                Number(prix.trim()).toLocaleString(
+                                var pv = data.pv.replace(` ${dev}`, '');
+                                var prix_min = data.prix_min.replace(` ${dev}`, '');
+
+                                prix = Number(prix.trim()).toLocaleString(
                                     'fr-FR', {
                                         minimumFractionDigits: 2
                                     });
-                                var prix_min = data.prix_min.replace(` ${dev}`, '');
-                                Number(prix_min.trim()).toLocaleString(
+                                prix_min = Number(prix_min.trim()).toLocaleString(
+                                    'fr-FR', {
+                                        minimumFractionDigits: 2
+                                    });
+                                pv = Number(pv.trim()).toLocaleString(
                                     'fr-FR', {
                                         minimumFractionDigits: 2
                                     });
                                 data.prix_min = prix_min + " " + dev;
                                 data.prix = prix + " " + dev;
+                                data.pv = pv + " " + dev;
 
                                 var tr = $('[tr-item=' + data.id + ']');
                                 var iqte = $('[item-qte-' + data.id + ']', tr);
